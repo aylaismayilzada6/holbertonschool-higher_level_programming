dfa9fe8d6f2eb73445518f69787ca05fbc314e68
Ayla Ismayilzada <150772989+aylaismayilzada6@users.noreply.github.com>
2026-03-21 19:15:11 +0400

    Update 9-print_last_digit.py

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,6 @@
-  #!/usr/bin/python3
+#!/usr/bin/python3
 def print_last_digit(number):
-    last_d = abs(number) % 10 
+    last_digit = abs(number) % 10
     print("{}".format(last_digit), end="")
-    return last_d
-
+    return last_digit
 
