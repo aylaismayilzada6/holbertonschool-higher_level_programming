87fcbea219118383f2d7e16458e0ff790f895615
root <aismayilzada19980@ada.edu.az>
2026-03-21 17:14:09 +0000

    task 3

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    def print_reversed_list_integer(my_list=[]):
+    temp = my_list.copy()
+    temp.reverse()
+    for num in temp:
+        print("{:d}".format(num))
