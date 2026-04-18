e5bdfbdc30ea4b65159c9c375312a6721ba5b040
aylaismayilzada6 <aismayilzada19980@ada.edu.az>
2026-04-18 16:31:11 +0000

    tasks

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module for appending a string to a text file.
+
+This module provides a function to append a
+string to the end of a text file (UTF-8)
+and return the number of characters added.
+"""
+def append_write(filename="", text=""):
+    """
+    Appends a string at the end of a text file
+    (UTF-8) and returns the number of characters added.
+
+    Args:
+        filename (str): The name of the file to
+        append to.
+        text (str): The text to append to the
+        file.
+
+    Returns
+        int: The number of characters added.
+    Raises:
+        OSError: If the file cannot be opened or
+        written.
+    Example:
+        >>> append_write('my_file.txt', 'Hello!')
+        6
+    """
+    with open(filename, 'a', encoding="utf-8") as f:
+        return f.write(text)
