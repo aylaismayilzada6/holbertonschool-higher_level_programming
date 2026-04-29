bd99974414dfe44467612592ac4c6f7396d6bf19
aylaismayilzada6 <aismayilzada19980@ada.edu.az>
2026-04-29 11:39:39 +0000

    tasks

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,8 @@
--- Creates the MySQL server user user_0d_1 with all privileges
+-- Creates the MySQL server user user_0d_1
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grants all privileges on all databases and tables
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Ensures that the privileges are applied immediately
+FLUSH PRIVILEGES;
