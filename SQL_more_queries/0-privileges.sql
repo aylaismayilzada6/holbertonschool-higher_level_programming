179f2be7ac895f07625c6ad9f32877bc018aaf78
aylaismayilzada6 <aismayilzada19980@ada.edu.az>
2026-04-29 11:35:24 +0000

    tasks

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Lists all privileges for user_0d_1 on localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Lists all privileges for user_0d_2 on localhost
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- Creates the database hbtn_0d_2 and the user user_0d_2
+-- User has only SELECT privilege in the database hbtn_0d_2
+-- Password is set to user_0d_2_pwd
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
